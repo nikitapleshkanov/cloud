79857f933c10dcf5ec83548448de1db935b2df62
nikitapleshkanov <nikitapleshkanovttt@mail.ru>
2019-12-17 12:20:27 +0300

    jscb

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def main():
 @app.route('/result')
 def wait():
     res = list(get_mongo_db().strings.find().sort([('$natural', -1)]).limit(1))
+    print(res)
     return render_template("result.html", answer=res[0]['result'])
 
 @app.route('/add_message', methods=['POST'])
